6ae515afabe02b6bdf0a74022d49194637706431
krprince421 <61113784+krprince421@users.noreply.github.com>
2022-04-17 21:59:09 +0530

    Add files via upload

---
+++ int221/app/Http/Controllers/FormController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Response ;
+
+class FormController extends Controller
+{
+    //
+    public function result( Request $request ){
+        echo "This is html result Function . " ;
+    }
+}
